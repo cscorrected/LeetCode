dbc4336d9c54e2bc511571099181590d5894c9ec
cscorrected <34055774+cscorrected@users.noreply.github.com>
2017-11-30 14:18:38 +0800

    Update 012_Integer_to_Roman_2.cpp

--- 012_Integer_to_Roman_2.cpp
+++ 012_Integer_to_Roman_2.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        const vector<int> vals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-        const vector<string> strs = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        static const vector<int> vals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        static const vector<string> strs = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
         string res;
         for (int i = 0; i < vals.size(); ++i) {
             int n = num / vals[i];
