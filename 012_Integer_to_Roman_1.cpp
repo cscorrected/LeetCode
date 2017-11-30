4cfaabcc8d21d82309e18639b47665093a18bd96
cscorrected <34055774+cscorrected@users.noreply.github.com>
2017-11-30 14:17:50 +0800

    Update 012_Integer_to_Roman_1.cpp

--- 012_Integer_to_Roman_1.cpp
+++ 012_Integer_to_Roman_1.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        const vector<string> M = { "", "M", "MM", "MMM" };
-        const vector<string> C = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-        const vector<string> X = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-        const vector<string> I = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        static const vector<string> M = { "", "M", "MM", "MMM" };
+        static const vector<string> C = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        static const vector<string> X = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        static const vector<string> I = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
         return M[num / 1000 % 10] + C[num / 100 % 10] + X[num / 10 % 10] + I[num % 10];
     }
 };
